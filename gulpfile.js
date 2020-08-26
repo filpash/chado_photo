8a585cd22ccabe033659733e5dd0fe919509e58b
Filipp <fili92@mail.ru>
2020-08-26 20:15:08 +0300

    prod change

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ var FAVICON_DATA_FILE = 'faviconData.json';
 
 gulp.task('generate-favicon', function(done) {
     realFavicon.generateFavicon({
-        masterPicture: 'src/assets/icons/Logo.png',
+        masterPicture: 'src/assets/icons/logofavi.png',
         dest: 'dist/assets/icons',
         iconsPath: './assets/icons',
         design: {
@@ -87,19 +87,12 @@ gulp.task('generate-favicon', function(done) {
     });
 });
 
-// Inject the favicon markups in your HTML pages. You should run
-// this task whenever you modify a page. You can keep this task
-// as is or refactor your existing HTML pipeline.
 gulp.task('inject-favicon-markups', function() {
     return gulp.src(['dist/*.html', 'dist/misc/*.html'])
         .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
         .pipe(gulp.dest('dist'));
 });
 
-// Check for updates on RealFaviconGenerator (think: Apple has just
-// released a new Touch icon along with the latest version of iOS).
-// Run this task from time to time. Ideally, make it part of your
-// continuous integration system.
 gulp.task('check-for-favicon-update', function(done) {
     var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
     realFavicon.checkForUpdates(currentVersion, function(err) {
